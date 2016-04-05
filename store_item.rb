a0139162ef808419252611dc62fcc78379ed4d6d
Ryan Meier <rymei10@gmail.com>
2016-04-04 20:55:03 -0700

    created file

---
+++ store_item.rb
@@ -0,0 +1,20 @@
+
+
+
+candles = [
+  {
+    :color => "red",
+    :scent=> "$40"
+    },
+   {
+   :color => "brown",
+   :scent => "$45"
+   },
+   {
+   :color=> "yellow",
+   :scent => "$10"
+   }
+ ]
+
+p candles
+       puts "#{candles}"
